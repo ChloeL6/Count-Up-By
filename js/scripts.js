67db3f5738c6380184de5347243e64530a814932
Chloe Le <yenchi.le1199@gmail.com>
2022-10-26 11:24:36 -0700

    Add Js and update BI in Js
    Co-authored-by: Brian Noh <noh24@ymail.com>

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+//business
+const countTo = 30; //this can change based on user input
+const countBy = 3; //this can change based on user input
+let numberArr = []; 
+let result = 0;
+
+for (let index = 1; result < countTo; index++) {
+  //let total = countby * index 
+  let total = countBy * index;
+  result = countBy * index;
+  //push value ^ to numberArr
+  console.log(result)
+  numberArr.push(total);
+  numberArr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// choose 2 numbers
+// number to count to: 10; user inputs
+// number to count by: 2; user inputs
+// output should count by 2 to reach 10 *maybe an array*
