ac3dea9b25a8b09ef2eaeda8027b7e0b4bef0bb8
Chloe Le <yenchi.le1199@gmail.com>
2022-10-26 12:02:51 -0700

    Add BI to js
    Co-authored-by: Brian Noh <noh24@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -6,26 +6,33 @@ function countUp(countTo, countBy) {
   let numArr = []; 
   let result = 0;
 
-  for (let index = 1; result < countTo; index++) {
+  for (let index = 1; result <= countTo; index++) {
     result = countBy * index;
     numArr.push(result);
   }
-  return numArr;
+  return numArr.join(", ");
 }
 
 //UI Logic
+function hideResults() {
+  document.getElementById("output").innerText = null;
+}
+function handleEvent(e) {
+  e.preventDefault();
+  hideResults();
+  let countToNum = document.querySelector("input#count-to").value;
+  let countByNum = document.getElementById("count-by").value;
+  
+  let result = countUp(countToNum, countByNum);
+  
+  const output = document.getElementById("output");
+  output.innerText = "Results: " + (" " + result);
+  // const p = document.createElement("p");
+  // output.append(p);
+  // p.innerText = "Results: " + (" " + result);
+}
 
-
-
-
-
-
-
-
-
-
-// 
-
-
-
-
+window.addEventListener("load", function() {
+  document.querySelector("form").addEventListener("submit", handleEvent);
+});
+  
