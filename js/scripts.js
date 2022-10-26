4afb13047586d81780efbce577d29af88714dc46
Chloe Le <yenchi.le1199@gmail.com>
2022-10-26 11:43:16 -0700

    Add and modify html file
    Co-authored-by: Brian Noh <noh24@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,19 @@
 //business
-const countTo = 30; //this can change based on user input
-const countBy = 3; //this can change based on user input
-let numberArr = []; 
-let result = 0;
+const countTo = 30; // input form for user 
+const countBy = 3; // input form for user
 
-for (let index = 1; result < countTo; index++) {
-  //let total = countby * index 
-  let total = countBy * index;
-  result = countBy * index;
-  //push value ^ to numberArr
-  console.log(result)
-  numberArr.push(total);
-  numberArr;
+function countUp(countTo, countBy) {
+  let numArr = []; 
+  let result = 0;
+
+  for (let index = 1; result < countTo; index++) {
+    result = countBy * index;
+    numArr.push(result);
+  }
+  return numArr;
 }
 
+//UI Logic
 
 
 
@@ -24,14 +24,8 @@ for (let index = 1; result < countTo; index++) {
 
 
 
+// 
 
 
 
 
-
-
-
-// choose 2 numbers
-// number to count to: 10; user inputs
-// number to count by: 2; user inputs
-// output should count by 2 to reach 10 *maybe an array*
